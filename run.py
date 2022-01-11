2dc5666ebb3b8785d9cce730ee9702e7d45d4396
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-11 21:19:09 +0000

    addlinks and data for member pages

--- run.py
+++ run.py
@@ -8,30 +8,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title_page="Home")
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json","r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", title_page="About", company = data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", title_page="Contact")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html", title_page="careers")
-
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)#must be changed to fale before deployments of final site
+        debug=True)
+
